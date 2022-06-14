09656caa4abe03ce3dbd5f1875d0ee7a7350f71d
Yamil-Layicov <yamilbl@code.edu.az>
2022-06-14 15:17:05 +0400

    Task-3

--- Anyway.sql
+++ Anyway.sql
@@ -21,11 +21,15 @@ Create Table Phones(
    BrandId int Foreign Key References Brands(Id)
 )
 
-Select a.Name,b.Name From Nootbooks a Inner Join Brands b On a.BrandId=b.Id
+Select a.Name,b.Name BrandName From Nootbooks a  Join Brands b On a.BrandId=b.Id
 
-Select a.Name,b.Name From Phones a Inner Join Brands b On a.BrandId=b.Id
+Select a.Name,b.Name BrandName From Phones a  Join Brands b On a.BrandId=b.Id
 
 Select * From Nootbooks Where Price Between 2000 And 5000 Or Price > 5000
 
 Select * From Phones Where Price Between 1000 And 1500 Or Price > 1500
+
+Select b.Name, Count(*)  From Brands b
+Join Nootbooks n on n.BrandId=b.Id
+Group By b.Name
 
